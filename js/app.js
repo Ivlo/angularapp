f520e97df73ad52bcf15cef0ab1ee47570aade11
Iván Lòpez <ivlo@MacBook-de-Ivan.local>
2015-08-09 11:56:50 +0200

    add pagination

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- var app = angular.module("myApp",['ngRoute']);
+ var app = angular.module("myApp",['ngRoute','angularUtils.directives.dirPagination']);
 
  app.config(['$routeProvider',function($routeProvider){
     $routeProvider.when('/',{
